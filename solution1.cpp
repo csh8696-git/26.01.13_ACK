92fd932976e851ea05d7c08163b7548654ee01c7
csh8696-git <csh8696@gmail.com>
2026-01-13 12:49:46 +0900

    solution2_skell,1_add
    41번 뼈대, 1번 타인 풀이 첨가

--- solution1.cpp
+++ solution1.cpp
@@ -23,3 +23,31 @@ int solution(int n) {
 	}
 	return answer;
 }
+
+int solution(int n) {
+	int answer = 0;
+	vector<int> v;
+	while (n > 0) {
+		v.push_back(n % 3);
+		n /= 3;
+	}
+	int k = 1;
+	while (!v.empty()) {
+		answer += k * v.back();
+		v.pop_back();
+		k *= 3;
+	}
+
+	return answer;
+}
+int solution(int n) {
+	vector<int> a;
+	while (n) {
+		a.push_back(n % 3);
+		n /= 3;
+	}
+
+	int ret = 0;
+	for (int i = 0; i < a.size(); ++i) ret = ret * 3 + a[i];
+	return ret;
+}
---
+++ solution2.cpp
@@ -0,0 +1,36 @@
+﻿#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string s) {
+	string answer = "";
+	stringstream line(s);
+	string word;
+	vector<string> words;
+	while (getline(line, word, ' '))
+	{
+		words.push_back(word);
+	}
+	for (int i = 0; i < words.size(); i++)
+	{
+		for (int j = 0; j < words[i].size(); j++)
+		{
+			if (j % 2 == 0)
+			{
+				words[i][j] = toupper(words[i][j]);
+			}
+			else
+			{
+				words[i][j] = tolower(words[i][j]);
+			}
+			if (i+1 < words.size())
+			{
+				words.push_back(" ");
+			}
+		}
+	}
+	return answer;
+}
