8f5886aa103da10288d934c041235208ad4e6bce
csh8696-git <csh8696@gmail.com>
2026-01-14 09:29:11 +0900

    solution2_41
    14일자 풀이->
    이상한 문자 만들기

--- solution2.cpp
+++ solution2.cpp
@@ -1,36 +1,73 @@
 ﻿#include <string>
 #include <vector>
-#include <sstream>
-#include <algorithm>
+#include <cctype>
 
 using namespace std;
 
 string solution(string s) {
-	string answer = "";
-	stringstream line(s);
-	string word;
-	vector<string> words;
-	while (getline(line, word, ' '))
+	string answer = s;
+	int index = 0;
+
+	for (int i = 0; i < s.size(); i++)
 	{
-		words.push_back(word);
-	}
-	for (int i = 0; i < words.size(); i++)
-	{
-		for (int j = 0; j < words[i].size(); j++)
+		if (s[i] == ' ')
 		{
-			if (j % 2 == 0)
-			{
-				words[i][j] = toupper(words[i][j]);
-			}
+			index = 0;
+			answer[i] = ' ';
+		}
+		else
+		{
+			if (index % 2 == 0)
+				answer[i] = toupper(s[i]);
 			else
-			{
-				words[i][j] = tolower(words[i][j]);
-			}
-			if (i+1 < words.size())
-			{
-				words.push_back(" ");
-			}
+				answer[i] = tolower(s[i]);
+			index++;
 		}
 	}
+
+	return answer;
+}
+
+string solution(string s) {
+	string answer = "";
+	int nIndex = 1;
+	for (int i = 0; i < s.size(); i++, nIndex++)
+	{
+		if (s[i] == ' ')
+		{
+			nIndex = 0;
+			answer += " ";
+		}
+		else
+			nIndex & 1 ? answer += toupper(s[i]) : answer += tolower(s[i]);
+	}
+
+	return answer;
+}
+
+string solution(string s) {
+	string answer;
+	int c = 2;
+	//모르겠다 꾀꼬리 꾀꼬리 나는야 아기상어
+	 //모르겟다 꾀꼬리 꾀꼬리 꾀꼬리 문자열 어렵따
+	int i;
+	for (i = 0; i <= s.size(); i++) {
+		if (c % 2 == 0) {
+			if (s[i] >= 97 && s[i] <= 122)
+				s[i] = s[i] - 32;
+			else if (s[i] == 32)
+				c = 1;
+
+		}
+		else {
+			if (s[i] >= 65 && s[i] <= 90)
+				s[i] = s[i] + 32;
+			else if (s[i] == 32)
+				c = 1;
+		}
+		c++;
+	}
+	answer = s;
+
 	return answer;
 }
